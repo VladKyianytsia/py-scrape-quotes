1b26daa9eea50322cc648c56ea9eb7ca7fb914b3
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-04-18 09:18:10 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,9 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
 
 
 @dataclass
@@ -8,8 +13,56 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+BASE_URL = "https://quotes.toscrape.com/"
+
+
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=quote_soup.select_one(".tags").text.split()[1:]
+    )
+
+
+def get_quotes_from_one_page(page_soup: BeautifulSoup) -> [Quote]:
+    quotes = page_soup.select(".quote")
+
+    return [parse_single_quote(quote_soup) for quote_soup in quotes]
+
+
+def get_all_quotes() -> [Quote]:
+    first_page = requests.get(BASE_URL).content
+    first_page_soup = BeautifulSoup(first_page, "html.parser")
+
+    all_quotes = get_quotes_from_one_page(first_page_soup)
+
+    page_number = 2
+    while True:
+        page_url = urljoin(BASE_URL, f"page/{page_number}")
+        page = requests.get(page_url).content
+        page_soup = BeautifulSoup(page, "html.parser")
+        all_quotes.extend(get_quotes_from_one_page(page_soup))
+
+        if not page_soup.select_one(".next"):
+            break
+
+        page_number += 1
+
+    return all_quotes
+
+
+def write_quotes_to_csv(quotes: [Quote], csv_path: str) -> None:
+    with open(csv_path, "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_quotes()
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":
